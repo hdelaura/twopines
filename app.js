9945246b4f24a091f6bb2736d622b9edc9e87f27
Haley DeLaura <d00266543@dmail.dixie.edu>
2019-04-10 18:09:46 -0600

    cleaning up files

--- app.js
+++ app.js
@@ -11,3 +11,25 @@ const app = new Vue ({
     methods: {
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    
+    if ($navbarBurgers.length > 0) {
+    
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+            
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+    
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+    
+        });
+      });
+    }
+    
+    });
