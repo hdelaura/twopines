00a599a8f82d1c44042d9131b9fd28f0268c087a
Haley DeLaura <d00266543@dmail.dixie.edu>
2019-04-10 21:44:11 -0600

    registered components, added data for contact form

--- public/app.js
+++ public/app.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 
-var app = new Vue ({
+Vue.component('calendar', require('../src/components/calendar.vue'));
+Vue.component('contact', require('../src/components/contact.vue'));
+Vue.component('footer', require('../src/components/footer.vue'));
+Vue.component('home', require('../src/components/home.vue'));
+Vue.component('nav', require('../src/components/nav.vue'));
+Vue.component('rentals', require('../src/components/rentals.vue'));
+
+const routes = {
+    '/': Home,
+    '/about': About,
+    '/contact': Contact,
+    '/gear': Gear,
+    '/booking': Booking,
+    '/howitworks': HowItWorks
+  }
+
+
+const app = new Vue ({
     el: '#app',
+    el: '#contactform',
     data: { 
         showHome: true,
         showGear: false,
         showHowItWorks: false,
         showAbout: false,
         showContact: false,
-        showCart: false
+        showCart: false,
+        user: {
+            name: '',
+            email: '',
+            message: ''
+        }
     },
     methods: {
+        handleSubmit() {
+            // Send data to the server or update your stores and such.
+        }
     }
 });
--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,30 +1,35 @@
 <template>
-    <div class="container" v-if="showContact">
+    <div class="container">
         <section class="is-fullheight-with-navbar">
             <div id="gear-padding">
                 <div class="columns">
+                    <form @submit.prevent="handleSubmit" id="contactform">
+                        <div class="column is-half">
+                            <div class="field">
+                                <label class="label">Name</label>
+                                <div class="control">
+                                    <input class="input" type="text" placeholder="Name" v-model="user.name">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Email</label>
+                                <div class="control">
+                                    <input class="input" type="email" placeholder="Email" v-model="user.email">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Message</label>
+                                <div class="control">
+                                    <textarea class="textarea" type="text" placeholder="Message" v-model="user.message"></textarea>
+                                </div>
+                            </div>
+                            <div class="control">
+                                <button class="button is-link" type="submit">Submit</button>
+                            </div>
+                        </div>
+                    </form>
                     <div class="column is-half">
-                        <div class="field">
-                            <label class="label">Name</label>
-                            <div class="control">
-                                <input class="input" type="text" placeholder="Name">
-                            </div>
-                        </div>
-                        <div class="field">
-                            <label class="label">Email</label>
-                            <div class="control">
-                                <input class="input" type="text" placeholder="Email">
-                            </div>
-                        </div>
-                        <div class="field">
-                            <label class="label">Message</label>
-                            <div class="control">
-                                <textarea class="textarea" placeholder="Message"></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="column is-half">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d101039.41048252548!2d-113.17209032501226!3d37.684887739071605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80b5617fe05ec2d1%3A0xef14cae28073d1e9!2sCedar+City%2C+UT!5e0!3m2!1sen!2sus!4v1554684202521!5m2!1sen!2sus" width="auto" height="800px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d101039.41048252548!2d-113.17209032501226!3d37.684887739071605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80b5617fe05ec2d1%3A0xef14cae28073d1e9!2sCedar+City%2C+UT!5e0!3m2!1sen!2sus!4v1554684202521!5m2!1sen!2sus" width="auto" height="800px" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
                 </div>
             </div>
