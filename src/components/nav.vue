77377d92f602821435d9f2faa352e82f3dcfbcaa
Haley DeLaura <d00266543@dmail.dixie.edu>
2019-04-10 18:41:18 -0600

    moving file contents

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -35,3 +35,26 @@
     </div>
 </nav>
 </template>
+
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    
+    if ($navbarBurgers.length > 0) {
+    
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+            
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+    
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+    
+        });
+      });
+    }
+});
+</script>
