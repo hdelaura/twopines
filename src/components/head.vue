e02641b0957a739e586779ced98fd5e96397403e
Haley DeLaura <d00266543@dmail.dixie.edu>
2019-04-10 18:24:44 -0600

    creating routes

---
+++ src/components/head.vue
@@ -0,0 +1,13 @@
+<template>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Two Pines Outfitters</title>
+    <link rel="stylesheet" href="css/mystyles.css">
+    <link rel="icon" href="images/icon.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://unpkg.com/vuex"></script>
+    <script type="text/javascript" src="lib/vue-hotel-datepicker.umd.min.js"></script>
+  </head>
+</template>
---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<script>
+import head from '../component/head.vue'
+import nav from '../component/nav.vue'
+import home from '../component/home.vue'
+import footer from '../component/footer.vue'
+</script>
+
+<template>
+<body>
+    <head></head>
+    <nav></nav>
+    <home></home>
+    <footer></footer>
+</body>
+</template>
