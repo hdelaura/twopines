d227355321c45b025686f9f8c59ac8fe10dfd359
Haley DeLaura <d00266543@dmail.dixie.edu>
2019-04-16 15:06:53 -0600

    Adding gear

--- src/components/rentals.vue
+++ src/components/rentals.vue
@@ -9,7 +9,10 @@
                 </div>
                 <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
                     <p>2 Person Tent</p>
-                    <p>Starting at $29.99/night</p>
+                    <p class="has-text-weight-semibold">Starting at $29.99/night</p>
+                    <div class="control">
+                        <button class="button is-small is-danger is-outlined" type="submit">Book Now</button>
+                    </div>
                 </div>
 
                 <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
@@ -17,7 +20,10 @@
                 </div>
                 <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
                     <p>4 Person Tent</p>
-                    <p>Starting at $59.99/night</p>
+                    <p class="has-text-weight-semibold">Starting at $59.99/night</p>
+                    <div class="control">
+                        <button class="button is-small is-danger is-outlined" type="submit">Book Now</button>
+                    </div>
                 </div>
             </div>
 
@@ -27,16 +33,70 @@
                 </div>
                 <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
                     <p>6 Person Tent</p>
-                    <p>Starting at $79.99/night</p>
+                    <p class="has-text-weight-semibold">Starting at $79.99/night</p>
+                    <div class="control">
+                        <button class="button is-small is-danger is-outlined" type="submit">Book Now</button>
+                    </div>
                 </div>
 
                 <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
-                        <figure class="image is-3by2"><img src="../../public/assets/6tent.jpg"></figure>
+                        <figure class="image is-3by2"><img src="../../public/assets/8tent.jpg"></figure>
                     </div>
                     <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
                         <p>8-10 Person Tent</p>
-                        <p>Starting at $119.99/night</p>
+                        <p class="has-text-weight-semibold">Starting at $119.99/night</p>
+                    <div class="control">
+                        <button class="button is-small is-danger is-outlined" type="submit">Book Now</button>
+                    </div>
+                    </div>
+            </div>
+
+            <div class="columns">
+                <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
+                    <figure class="image is-1by1"><img src="../../public/assets/mummybag.jpg"></figure>
                 </div>
+                <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
+                    <p>1 Person Mummy Bag</p>
+                    <p class="has-text-weight-semibold">Starting at $9.99/night</p>
+                    <div class="control">
+                        <button class="button is-small is-danger is-outlined" type="submit">Book Now</button>
+                    </div>
+                </div>
+
+                <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
+                        <figure class="image is-1by1"><img src="../../public/assets/2mummybag.jpg"></figure>
+                    </div>
+                    <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
+                        <p>2 Person Mummy Bag</p>
+                        <p class="has-text-weight-semibold">Starting at $19.99/night</p>
+                    <div class="control">
+                        <button class="button is-small is-danger is-outlined" type="submit">Book Now</button>
+                    </div>
+                    </div>
+            </div>
+
+            <div class="columns">
+                <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
+                    <figure class="image is-1by1"><img src="../../public/assets/kitchen.jpg"></figure>
+                </div>
+                <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
+                    <p>Kitchen Set</p>
+                    <p class="has-text-weight-semibold">Starting at $12.99/night</p>
+                    <div class="control">
+                        <button class="button is-small is-danger is-outlined" type="submit">Book Now</button>
+                    </div>
+                </div>
+
+                <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
+                        <figure class="image is-1by1"><img src="../../public/assets/chairs.jpg"></figure>
+                    </div>
+                    <div class="column is-full-mobile is-half-tablet is-one-quarter-desktop is-one-quarter-fullhd">
+                        <p>Camping Chairs</p>
+                        <p class="has-text-weight-semibold">Starting at $5.99/night</p>
+                    <div class="control">
+                        <button class="button is-small is-danger is-outlined" type="submit">Book Now</button>
+                    </div>
+                    </div>
             </div>
         </div>
     </section>
